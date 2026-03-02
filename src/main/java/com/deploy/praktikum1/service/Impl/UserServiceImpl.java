5c26586e4fd2205ea1d439d58bccf82cc7594221
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-03-02 11:30:21 +0700

    Memperbaiki error

--- src/main/java/com/deploy/praktikum1/service/Impl/UserServiceImpl.java
+++ src/main/java/com/deploy/praktikum1/service/Impl/UserServiceImpl.java
@@ -8,11 +8,13 @@ import com.deploy.praktikum1.repository.UserRepository;
 import com.deploy.praktikum1.service.UserService;
 import com.deploy.praktikum1.util.ValidationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
